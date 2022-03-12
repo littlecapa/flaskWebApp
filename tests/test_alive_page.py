1c9dcfe5c4ba6c824820db654d5a69e75c4700be
littlecapa <littlecapa@Rogers-Mac-min.fritz.box>
2022-03-12 15:08:45 +0100

    Unit Tests

--- tests/test_alive_page.py
+++ tests/test_alive_page.py
@@ -1,14 +1,21 @@
 import unittest
 
+import os, sys, inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+from app import app
+
 class BasicTests(unittest.TestCase):
 
     def setUp(self):
-        print("Start")
+        #print("Start")
 
     def test_alive_page(self):
 
-        response = self.app.get("/alive")
-        assert response.status_code != 200
+        response = app.test_client().get("/alive")
+        assert response.status_code == 200
         assert b"About This App" in response.data
 
 if __name__ == "__main__":
